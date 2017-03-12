07b84e0ba02246f52b987b23e085dddcfeb6434f
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeiMac-3.local>
2017-03-12 20:23:33 +0800

    基础化

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loanAmount: UITextField!
+    @IBOutlet weak var loanTerm: UITextField!
+    @IBOutlet weak var interestRate: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBAction func simpleCaluculate(_ sender: Any) {
+        let caluculate1 = simpleCalculate()
+        result.text=caluculate1.calculate(loanAmount: Double(loanAmount.text!)!, loanTerm: Int(loanTerm.text!)!, interestRate: Double(interestRate.text!)!).description
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    class simpleCalculate{
+        func calculate(loanAmount:Double,loanTerm:Int,interestRate:Double) -> Double{
+        let rate = interestRate/100
+        let interest = loanAmount * rate * Double(loanTerm)
+        return loanAmount + interest
+        }
+    }
 
 }
 
