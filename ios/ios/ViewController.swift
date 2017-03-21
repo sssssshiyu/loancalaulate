efea47673b50294e7a31c86d233c0bacf6ea89bf
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeiMac-3.local>
2017-03-21 21:05:54 +0800

    改

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -24,17 +24,13 @@ class ViewController: UIViewController {
                 print("用户没有输入全部信息")
                 return
         }
-        
         guard let loanterm = Int(loanTerm),let loanamount = Double(loanAmount),let interestrate = Double(interestRate)
             else {
                 print("用户输入数据格式有错")
-                return
-            
+             return
         }
         result.text=caluculate1.calculate(loanAmount:loanamount, loanTerm: loanterm, interestRate: interestrate).money
     }
-    
-    
     @IBAction func compoundButton(_ sender: Any) {
         let caluculate2 = compoundCaluculate()
         guard let loanTerm = loanTerm.text,let loanAmount = loanAmount.text,let interestRate = interestRate.text
@@ -42,41 +38,27 @@ class ViewController: UIViewController {
                 print("用户没有输入全部信息")
                 return
         }
-        
         guard let loanterm = Int(loanTerm),let loanamount = Double(loanAmount),let interestrate = Double(interestRate)
             else {
                 print("用户输入数据格式有错")
                 return
-                
         }
         result.text=caluculate2.calculate(loanAmount:loanamount, loanTerm: loanterm, interestRate: interestrate).money
     }
-
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         loanAmount.resignFirstResponder()
         loanTerm.resignFirstResponder()
         interestRate.resignFirstResponder()
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
 }
-
-
 
 class simpleCalculate{
         func calculate(loanAmount:Double,loanTerm:Int,interestRate:Double) -> Double{
@@ -85,6 +67,7 @@ class simpleCalculate{
         return loanAmount + interest
         }
     }
+
 class compoundCaluculate{
         func calculate(loanAmount:Double,loanTerm:Int,interestRate:Double) -> Double{
         let rate = interestRate/100
@@ -92,8 +75,6 @@ class compoundCaluculate{
         return loanAmount * compoundinterest
         }
     }
-
-
 
 extension Double{
     var money:String{
