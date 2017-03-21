95c4885d0bad999982b9c5b3025154dbb58d2a69
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeiMac-3.local>
2017-03-21 20:21:45 +0800

    改

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -19,15 +19,39 @@ class ViewController: UIViewController {
     
     @IBAction func simpleButton(_ sender: Any) {
         let caluculate1 = simpleCalculate()
-        result.text=caluculate1.calculate(loanAmount: Double(loanAmount.text!)!, loanTerm: Int(loanTerm.text!)!, interestRate: Double(interestRate.text!)!).description
+        guard let loanTerm = loanTerm.text,let loanAmount = loanAmount.text,let interestRate = interestRate.text
+            else{
+                print("用户没有输入全部信息")
+                return
+        }
+        
+        guard let loanterm = Int(loanTerm),let loanamount = Double(loanAmount),let interestrate = Double(interestRate)
+            else {
+                print("用户输入数据格式有错")
+                return
+            
+        }
+        result.text=caluculate1.calculate(loanAmount:loanamount, loanTerm: loanterm, interestRate: interestrate).money
     }
-    
     
     
     @IBAction func compoundButton(_ sender: Any) {
         let caluculate2 = compoundCaluculate()
-        result.text=caluculate2.calculate(loanAmount: Double(loanAmount.text!)!, loanTerm: Int(loanTerm.text!)!, interestRate: Double(interestRate.text!)!).description
+        guard let loanTerm = loanTerm.text,let loanAmount = loanAmount.text,let interestRate = interestRate.text
+            else{
+                print("用户没有输入全部信息")
+                return
+        }
+        
+        guard let loanterm = Int(loanTerm),let loanamount = Double(loanAmount),let interestrate = Double(interestRate)
+            else {
+                print("用户输入数据格式有错")
+                return
+                
+        }
+        result.text=caluculate2.calculate(loanAmount:loanamount, loanTerm: loanterm, interestRate: interestrate).money
     }
+
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         loanAmount.resignFirstResponder()
